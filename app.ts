f0d6ed1abd3f71a6c32e6d398808525beb6c5229
Abid Dar <abiddar1979@gmail.com>
2024-04-13 16:44:13 +0500

    files added

---
+++ app.ts
@@ -0,0 +1,10 @@
+/* Task # 12 : Greetings : Start with the array you used in the Exercise 11, but instead of just printing each person's name, print a massage to them . The text of eachmassage should be the same, but each massage should be  personalized with the person's name.*/
+
+
+let names: string[] = ["Sabtain", "Sufian", "Yaseen", "Shuaib"];
+
+
+console.log(`Make Sure To Subscribe To Our Channel, ${names[0]}`);
+console.log(`Make Sure To Subscribe To Our Channel, ${names[1]}`);
+console.log(`Make Sure To Subscribe To Our Channel, ${names[2]}`);
+console.log(`Make Sure To Subscribe To Our Channel, ${names[3]}`);
